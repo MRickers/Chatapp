93e7b38d86bb2d69db996c8721f8d1d1b5a1a1e5
mrickers <mrickers@linux.fritz.box>
2023-07-23 18:01:39 +0200

    Frontend added Bootstrap 5 CDN and Users Online

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 
 var upgrader = websocket.Upgrader{}
 var clients = make(map[*websocket.Conn]bool)
+var userNames = make(map[*websocket.Conn]string)
 
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
@@ -20,24 +22,56 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	// Prompt user for username and store it
+	conn.WriteMessage(websocket.TextMessage, []byte("Enter your username:"))
+	_, username, err := conn.ReadMessage()
+	if err != nil {
+		log.Println("Error reading username:", err)
+		return
+	}
+
+	userNames[conn] = string(username)
 	clients[conn] = true
 
+	updateUsersList()
+
+	// Handle chat messages
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println("Error reading message: ", err)
+			log.Println("Error reading message:", err)
 			break
 		}
-		log.Println("read message: ", p)
-		// Broadcast
-		for client := range clients {
-			log.Println("broadcasting message")
-			err := client.WriteMessage(messageType, p)
+
+		// Broadcast the received message to all connected clients
+		for clientConn := range clients {
+			err := clientConn.WriteMessage(messageType, p)
 			if err != nil {
 				log.Println("Error writing message:", err)
 				break
 			}
 		}
+	}
+
+	delete(clients, conn)
+	delete(userNames, conn)
+	updateUsersList()
+}
+
+func updateUsersList() {
+	userList := make([]string, 0, len(userNames))
+	for _, username := range userNames {
+		userList = append(userList, username)
+	}
+
+	userJSON, err := json.Marshal(userList)
+	if err != nil {
+		log.Println("Error marshaling user list:", err)
+		return
+	}
+
+	for clientConn := range clients {
+		clientConn.WriteMessage(websocket.TextMessage, userJSON)
 	}
 }
 
@@ -51,5 +85,5 @@ func main() {
 	r.HandleFunc("/", homeHandler)
 
 	fmt.Println("Chat Server started. Listening on port 8080...")
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
